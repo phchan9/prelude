6456272bdd4d6af57ba3b4970f06576474595c0f
phchan9 <phckhc@gmail.com>
2017-05-03 17:57:18 +0800

    add some package and tune js package

--- personal/ph-custom.el
+++ personal/ph-custom.el
@@ -1,5 +1,5 @@
 ;; required extra package
-(prelude-require-packages '(neotree))
+(prelude-require-packages '(neotree js2-refactor ag xref-js2))
 
 ;; set font size and font type
 (if (equal system-type 'darwin)
@@ -45,22 +45,39 @@ at right"
 (server-start)
 
 ;; js setting for emacs, Reference by http://prak5190.github.io/p/jsemacs/
-(require 'auto-complete)
-; do default config for auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
-;; start yasnippet with emacs
-(require 'yasnippet)
-(yas-global-mode 1)
+;; (require 'auto-complete)
+;; ; do default config for auto-complete
+;; (require 'auto-complete-config)
+;; (ac-config-default)
+;; ;; start yasnippet with emacs
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
 
-(add-hook 'js2-mode-hook 'ac-js2-mode)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(eval-after-load 'tern
-  '(progn
-     (require 'tern-auto-complete)
-     (tern-ac-setup)))
-(add-hook 'js2-mode-hook 'tern-mode)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+;; (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;; (eval-after-load 'tern
+;;   '(progn
+;;      (require 'tern-auto-complete)
+;;      (tern-ac-setup)))
+;; (add-hook 'js2-mode-hook 'tern-mode)
 
+
+;; add js2-refactor
+(require 'ag)
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook (lambda ()
+                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+;; better imenu for js2 mode
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
 (provide 'ph-custom)
 ;;; ph-custom.el ends here
